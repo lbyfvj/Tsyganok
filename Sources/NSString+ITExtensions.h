4368e8601af7bba05049f70aa711ea1ba9574995
Ivan Tsyganok <tsyganok.ivan@gmail.com>
2016-12-06 23:59:33 +0200

    HWT4 -> NSString extension added

---
+++ Sources/NSString+ITExtensions.h
@@ -0,0 +1,13 @@
+//
+//  NSString+ITExtensions.h
+//  HWL1T1
+//
+//  Created by Ivan Tsyganok on 06.12.16.
+//  Copyright © 2016 Ivan Tsyganok. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (ITExtensions)
+
+@end
