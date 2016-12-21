80fffcabc4c35112382a424ccabe02e0012835f2
Ivan Tsyganok <tsyganok.ivan@gmail.com>
2016-12-21 21:42:59 +0200

    ITCarWash initial commit

---
+++ ITCarWash/ITCarWash/ITCarWash/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ITCarWash
+//
+//  Created by Ivan Tsyganok on 21.12.16.
+//  Copyright © 2016 Ivan Tsyganok. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
